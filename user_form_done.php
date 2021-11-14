5b8ad1da05b83c366e65896908321049e4095180
masakifuse <masakifuse1021@gmail.com>
2021-11-15 03:39:49 +0900

    サイト制作

---
+++ user_form_done.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+session_regenerate_id(true);
+$_SESSION=array();
+if(isset($_COOKIE[session_name()])==true)
+{
+  setcookie(session_name(),'',time()-4200,'/');
+}
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ホームページ制作サイト</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
+
+try {
+  require_once('../common/common.php');
+  $post = sanitize($_POST);
+  $onamae = $post['onamae'];
+  $furigana = $post['furigana'];
+  $email = $post['email'];
+  $address = $post['address'];
+  $tel = $post['tel'];
+  $money = $post['money'];
+  $op = $post['op'];
+  $soudan = $post['soudan'];
+
+  echo $onamae . '様<br />';
+  echo 'ご依頼ありがとうございました。<br />';
+  echo $email . 'にメールを送りましたのでご確認願います。<br />';
+  echo '契約書・納品日につきましてはご相談の上決めさせていただきます。<br />';
+  echo $address . '<br />';
+  echo $tel . '<br />';
+  echo "金額：{$money}";
+  echo '<br /><br />';
+
+  $user_name = $_POST['onamae'];
+  $user_furigana = $_POST['furigana'];
+  $user_email = $_POST['email'];
+  $user_address = $_POST['address'];
+  $user_tel = $_POST['tel'];
+  $user_money = $_POST['money'];
+  $user_job = $_POST['op'];
+  $user_soudan = $_POST['soudan'];
+
+  $user_name = htmlspecialchars($user_name,ENT_QUOTES,'UTF-8');
+  $user_furigana = htmlspecialchars($user_furigana,ENT_QUOTES,'UTF-8');
+  $user_email = htmlspecialchars($user_email,ENT_QUOTES,'UTF-8');
+  $user_address = htmlspecialchars($user_address,ENT_QUOTES,'UTF-8');
+  $user_tel = htmlspecialchars($user_tel,ENT_QUOTES,'UTF-8');
+  $user_money = htmlspecialchars($user_money,ENT_QUOTES,'UTF-8');
+  $user_job = htmlspecialchars($user_job,ENT_QUOTES,'UTF-8');
+  $user_soudan = htmlspecialchars($user_soudan,ENT_QUOTES,'UTF-8');
+
+  $dsn = 'mysql:dbname=user;host=localhost;charset=utf8';
+  $user = 'root';
+  $password = '';
+  $dbh= new PDO($dsn,$user,$password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  
+  $sql = 'INSERT INTO mst_user(name,furigana,email,address,tel,money,job,soudan) 
+  VALUES(?,?,?,?,?,?,?,?)';
+  $stmt = $dbh->prepare($sql);
+  $data[] = $user_name;
+  $data[] = $user_furigana;
+  $data[] = $user_email;
+  $data[] = $user_address;
+  $data[] = $user_tel;
+  $data[] = $user_money;
+  $data[] = $user_job;
+  $data[] = $user_soudan;
+  $stmt->execute($data);
+  $dbh =null;
+  
+
+
+
+  $honbun ='';
+  $honbun.=$onamae."様 \n\n この度はご注文ありがとうございました。\n";
+  $honbun.="\n";
+  $honbun.="ご依頼内容\n";
+  $honbun.="----------------------\n";
+  $honbun.= $soudan;
+  $honbun.="\n";
+  $honbun.="----------------------\n";
+  $honbun.="代金は以下の口座にお振り込みください。\n";
+  $honbun.="りそな銀行長吉支店 普通口座6268008\n";
+  $honbun.="入金が取れ次第、早期に納品致します。\n";
+  $honbun.="\n";
+  $honbun.="□□□□□□□□□□□□□□□□□□□□\n";
+  $honbun.="~DIV Free fuse~\n";
+  $honbun.="\n";
+  $honbun.="代表：布施　正貴";
+  $honbun.="\n\n";
+  $honbun.="大阪市平野区瓜破東３丁目７−２１\n";
+  $honbun.="電話 080-3779-6356\n";
+  $honbun.="メール masakifuse1021@gmail.com\n";
+  $honbun.="□□□□□□□□□□□□□□□□□□□□\n";
+  echo'<br />';
+  echo nl2br($honbun);
+  $title= 'ご注文ありがとうございます。';
+  $header = 'From: masakifuse1021@gmail.com';
+  $honbun = html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
+  mb_language('Japanese');
+  mb_internal_encoding('UTF-8');
+  mb_send_mail($email,$honbun,$header);
+
+  $title= 'お客様から注文がありました。';
+  $header = 'From:'.$email;
+  $honbun = html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
+  mb_language('Japanese');
+  mb_internal_encoding('UTF-8');
+  mb_send_mail('masakifuse1021@gmail.com',$title,$honbun,$header);
+
+
+} catch (Exception $e) {
+  echo $e;
+  exit();
+}
+
+?>
+</body>
+
+</html>
--- user_login_check.php
+++ user_login_check.php
@@ -1,16 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>イベントサイト</title>
-</head>
-<body>
-  <?php
+<?php
   require_once('../common/common.php');
+$post=sanitize($_POST);
+$onamae=$post['onamae'];
+$furigana= $post['furigana'];
+$email= $post['email'];
+$address= $post['address'];
+$tel = $post['tel'];
+$money= $post['money'];
+$op= $post['op'];
+$soudan = $post['soudan'];
 
-  $post=sanitize($_POST);
+$okflg = true;
+
+if($onamae=='')
+{
+  echo 'お名前が入力されていません。<br/><br/>';
+  $okflg = false;
+}
+else
+{
+  echo 'お名前<br />';
+  echo $onamae;
+  echo '<br /><br />';
+}
+if($furigana=='')
+{
+  echo 'フリガナが入力されていません。<br /><br />';
+  $okflg = false;
+}
+else
+{
+  echo 'フリガナ<br />';
+  echo $furigana;
+  echo '<br /><br />';
+}
+if(preg_match('/\A[\w\-\.]+\@[\w\-\.]+\.([a-z]+)\z/',$email)==0)
+  {
+    echo 'メールアドレスを正確に入力してください。<br/><br />';
+    $okflg = false;
+  }
+  else
+  {
+    echo 'メールアドレス<br />';
+    echo $email;
+    echo '<br /><br />';
+  }
+
+  if($address=='')
+  {
+    echo '住所を入力してください。<br /><br />';
+    $okflg = false;
+  }
+  else
+  {
+    echo '住所<br />';
+    echo $address;
+    echo '<br /><br />';
+  }
+  if($tel=='')
+  {
+    echo '電話番号を入力してください。<br /><br />';
+    $okflg = false;
+  }
+  echo '電話番号<br />';
+  echo $tel;
+  echo '<br /><br />';
+
+  if($money=='')
+  {
+    echo '金額を半角数字で入力してください。<br /><br />';
+    $okflg = false;
+  }
+  if($money<50000)
+  if(preg_match('/^[0-9]+$/D', $money))
+  {
+
+    echo '(半角数字入力）金額は５００００万円以上で依頼願います。';
+  }
+  else
+  {
+    
+    echo "金額:{$money}";
+    echo '<br /><br />';
+  }
+  if($op=='')
+  {
+    echo '現在のお客様状況がチェックされていません。<br /><br />';
+  }
+  else
+  {
+    echo 'ご職業<br />';
+    echo $op;
+    echo '<br /><br />';
+  }
+  if(isset($_POST['soudan']))
+  {
+    echo 'ご相談<br />';
+    echo $soudan;
+    echo '<br /><br />';
+  }
+
+  if($okflg==true)
+  {
+  echo '<form method="post" action="user_form_done.php">';
+  echo '<input type="hidden" name="onamae" value="'.$onamae.'">';
+  echo '<input type="hidden" name="furigana" value="'.$furigana.'">';
+  echo '<input type="hidden" name="email" value="'.$email.'">';
+  echo '<input type="hidden" name="address" value="'.$address.'">';
+  echo '<input type="hidden" name="tel" value="'.$tel.'">';
+  echo '<input type="hidden" name="money" value="'.$money.'">';
+  echo '<input type="hidden" name="currentPosition" value="'.$currentPosition.'">';
+  echo '<input type="hidden" name="soudan" value="'.$soudan.'">';
+  echo '<input type="button" onclick="history.back()" value="戻る">';
+  echo '<input type="submit" value = "OK"><br />';
+  echo '</form>';
+  }
+  else
+  {
+    echo '<form>';
+    echo '<input type ="button" onclick= "history.back()" value="戻る">';
+  }
   ?>
 </body>
 </html>
